f9124e43462d1dd0d49f7c71491bd626a26e1eb1
Ridoy-paul <cse.ridoypaul@gmail.com>
2023-10-25 23:27:00 +0600

    updated

---
+++ lib/widgets/score_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ScoreWidget extends StatefulWidget {
+  const ScoreWidget({super.key});
+
+  @override
+  State<ScoreWidget> createState() => _ScoreWidgetState();
+}
+
+class _ScoreWidgetState extends State<ScoreWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
